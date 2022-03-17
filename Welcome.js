d53b30135ac4a7fb61ff328fab32a07321407ee2
Hussein Aref <Hussein.aref.csci@gmail.com>
2022-03-17 11:02:49 +0200

    getElementById

---
+++ Welcome.js
@@ -0,0 +1,4 @@
+//JavaScript Can Change HTML Content
+//One of many JavaScript HTML methods is getElementById().
+//The example below "finds" an HTML element (with id="demo"), and changes the element content (innerHTML) to "Hello JavaScript":
+document.getElementById('Title').innerHTML ='Welcome to JS Basics';
